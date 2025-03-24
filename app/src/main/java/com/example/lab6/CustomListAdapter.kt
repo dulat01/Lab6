134575d4817bc2105489c4fcc059eb530763218c
dulat01 <mdulat2004@mail.ru>
2025-03-24 11:01:51 +0500

    10

--- app/src/main/java/com/example/lab6/CustomListAdapter.kt
+++ app/src/main/java/com/example/lab6/CustomListAdapter.kt
@@ -25,37 +25,42 @@ class CustomListAdapter(private val context: Context, private val listData: List
         return position.toLong()
     }
 
-    override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
-        var convertView = convertView
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
         val holder: ViewHolder
-        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.list_item_layout, null)
+
+        if (view == null) {
+            view = layoutInflater.inflate(R.layout.list_item_layout, parent, false)
             holder = ViewHolder()
-            holder.flagView = convertView.findViewById<View>(R.id.imageView_flag) as ImageView
-            holder.countryNameView =
-                convertView.findViewById<View>(R.id.textView_countryName) as TextView
-            holder.populationView =
-                convertView.findViewById<View>(R.id.textView_population) as TextView
-            convertView.tag = holder
+            holder.flagView = view.findViewById(R.id.imageView_flag)
+            holder.countryNameView = view.findViewById(R.id.textView_countryName)
+            holder.populationView = view.findViewById(R.id.textView_population)
+            view.tag = holder
         } else {
-            holder = convertView.tag as ViewHolder
+            holder = view.tag as ViewHolder
         }
 
         val country = listData[position]
-        holder.countryNameView!!.text = country.countryName
-        holder.populationView!!.text = "Population: " + country.population
+        holder.countryNameView?.text = country.countryName
+        holder.populationView?.text = "Population: ${country.population}"
 
-        val imageId = this.getMipmapResIdByName(country.flagName)
-        holder.flagView!!.setImageResource(imageId)
+        val imageId = getMipmapResIdByName(country.flagName)
+        if (imageId != 0) {
+            holder.flagView?.setImageResource(imageId)
+        }
 
-        return convertView
+        return view!!
     }
 
     // Find Image ID corresponding to the name of the image (in the directory mipmap)
-    fun getMipmapResIdByName(resName: String?): Int {
+    private fun getMipmapResIdByName(resName: String?): Int {
         val pkgName = context.packageName
-        val resID = context.resources.getIdentifier(resName, "mipmap", pkgName)
-        return resID
+        // Добавляем проверку на null
+        return if (resName != null) {
+            context.resources.getIdentifier(resName, "mipmap", pkgName)
+        } else {
+            0
+        }
     }
 
     internal class ViewHolder {
